23ad12ca60eebdf1990515f4c8851ff52a6442cf
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-08-19 11:08:19 +0200

    Socket functions

--- Socket.h
+++ Socket.h
@@ -9,20 +9,24 @@
 #include <cstdio>
 #include <string>
 #include <memory>
-#include "SyncedFile.h"
-
-class string;
+#include <optional>
 
 class Socket {
 private:
     //todo: mettere una dimensione del buffer ragionevole
-    const static int N = 1024;
+    const static int N = 10240;
     int socket_fd;
+    const static int timeout_value = 20;
+
+    std::string username;
     Socket(int sockfd);
-    ssize_t read(char *buffer, size_t len);
-    ssize_t write(const char *buffer, size_t len);
-    void connect(struct sockaddr_in *addr, unsigned int len);
-//    friend class ServerSocket; //è friend perché voglio poter chiamare il suo costruttore privato
+    ssize_t read(char *buffer, size_t len) const;
+    ssize_t write(const char *buffer, size_t len) const;
+    void connect(struct sockaddr_in *addr, unsigned int len) const;
+    bool setReadSelect(fd_set &read_fds);
+    bool setWriteSelect(fd_set &write_fds);
+    bool sendResp(std::string resp);
+
 public:
     Socket &operator=(const Socket &) = delete; //elimino operatore di assegnazione
     Socket();
@@ -32,9 +36,23 @@ public:
 
     Socket &operator=(Socket &&other) noexcept ; // costruttore di copia per movimento
     void connectToServer(std::string address, int port);
-    void closeConnection();
-    void sendFile(const std::shared_ptr<SyncedFile>& syncedFile);
-    void sendJSON(const std::string &JSON);
+    void closeConnection() const;
+    bool sendFile(const std::shared_ptr<SyncedFileServer>& syncedFile);
+    std::optional<std::string> readJSON();
+    std::optional<std::string> getFile(int size);
+    void fileError();
+
+    [[nodiscard]] const std::string &getUsername() const;
+    void setUsername(std::string username);
+
+    static std::string tempFilePath();
+
+    bool sendOKResp();
+    bool sendKOResp();
+    bool sendNOResp();
+    std::optional<std::string> getResp();
+
+    bool sendJSON(const std::string& JSON);
 };
 
 
