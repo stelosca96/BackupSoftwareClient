2a86559e7fa4ca9158fc762719a4ba061a885e71
pierluigi <s214228@studenti.polito.it>
2020-09-27 16:29:49 +0200

    cmake + json config

---
+++ cmake-build-debug/CMakeFiles/FileWatcher.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/Client.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/Client.cpp.o"
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/FileWatcher.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/FileWatcher.cpp.o"
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/SyncedFile.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/SyncedFile.cpp.o"
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/UploadJobs.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/UploadJobs.cpp.o"
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/User.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/User.cpp.o"
+  "/home/pierluigi/CLionProjects/BackupSoftwareClient/main.cpp" "/home/pierluigi/CLionProjects/BackupSoftwareClient/cmake-build-debug/CMakeFiles/FileWatcher.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
